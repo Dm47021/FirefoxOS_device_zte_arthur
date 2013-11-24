709e8895827bc508027a1cb345d8ab4df456835d
Dm47021 <dm47021@hotmail.com>
2013-11-23 21:49:51 -0500

    initial cm10 tree as of M9

---
+++ cm.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := arthur
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/zte/arthur/full_arthur.mk)
+
+# Set up Propper APNS
+PRODUCT_COPY_FILES+= \
+    device/zte/arthur/prebuilt/files/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Hot reboot
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/cm/overlay/hot_reboot
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := arthur
+PRODUCT_NAME := cm_arthur
+PRODUCT_BRAND := BoostMobile
+PRODUCT_MODEL := N860
+PRODUCT_MANUFACTURER := ZTE
+PRODUCT_CHARACTERISTICS := phone
+
